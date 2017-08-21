0671842a0117b77d62555ed01702651d049d4443
Oli Lalonde <olalonde@gmail.com>
2017-08-21 15:12:22 -0700

    sanic: debug=False

--- servers/sanic/micro.py
+++ servers/sanic/micro.py
@@ -14,5 +14,10 @@ async def health(request):
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, workers=1)
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        workers=1,
+        debug=False
+    )
 
